4bde06b19576951ca73a38ac8c7c5876e3fd68c9
Juan M. Gonzalez R <juanml93@gmail.com>
2020-04-08 16:16:53 -0400

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TopPanel from './containers/TopPanel'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import HomePage from './pages/HomePage'
+
 
 function App() {
   return (
     <div className="App">
-      <TopPanel/>
-       <Router>
-          <LeftPanel open={this.state.open} handleDrawerClose={this.handleDrawerClose}/>
-          
-          <main className={classes.content}>
-            <div className={classes.appBarSpacer} />
-            
-              <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/interviews" exact component={InterviewsGrid} />
-                <Route path="/candidates" exact component={CandidatesGrid} />
-                <Route path="/interviewers" exact component={InterviewersGrid} />
-              </Switch>
-            
-            <div className={classes.tableContainer}>
-              
-            </div>
-          </main>
-        </Router>
+      <TopPanel />
+      <Router>
+        <LeftPanel open={this.state.open} handleDrawerClose={this.handleDrawerClose} />
+
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+          </Switch>
+
+          <div className={classes.tableContainer}>
+
+          </div>
+        </main>
+      </Router>
     </div>
   );
 }
