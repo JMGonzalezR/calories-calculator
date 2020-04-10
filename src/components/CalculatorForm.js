5f0339d05019b137bc009c84875d5eefddca542d
Juan M. Gonzalez R <juanml93@gmail.com>
2020-04-10 02:38:09 -0400

    work in progress adding the calories calculator form

---
+++ src/components/CalculatorForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CalculatorForm.css';
+
+function CalculatorForm() {
+  return (
+    <div className="CalculatorForm">
+        <button className="CalculatorForm-button"> Male </button>
+    </div>
+  );
+}
+
+export default CalculatorForm;
